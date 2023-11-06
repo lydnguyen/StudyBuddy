a2bf069d925fde0e97ec7ed768e870e12b3867dd
lanydnguyen <lan.y.d.nguyen@gmail.com>
2023-11-06 11:40:12 +0100

    Add replykeyboard options

--- telegram_bot/studybuddyapp.py
+++ telegram_bot/studybuddyapp.py
@@ -1,34 +1,30 @@
-import time
-import yaml
 import random
 import pyfiglet
 import logging
 import logging.config
-import sys
 import os
-from dotenv import load_dotenv, find_dotenv
-import telegram
 from _model import *
 from _authentications import Authenticate
 from _access_source import ListQuestionaire
 from telegram import (
-    KeyboardButton,
-    KeyboardButtonPollType,
     Poll,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
     Update,
 )
-from telegram.constants import ParseMode
 from telegram.ext import (
     Application,
     CommandHandler,
     ContextTypes,
-    MessageHandler,
-    PollAnswerHandler,
     PollHandler,
-    filters,
 )
+from telegram import (
+    ReplyKeyboardMarkup
+    , KeyboardButton
+)
+
+
+def help_command_handler(update: Update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text("Type /start")
 
 
 def get_question_object():
@@ -57,6 +53,7 @@ def questionaire_generator():
 
 async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a predefined poll"""
+    reply_keyboard = [['/help', '/start_quiz']]
     quiz_question = get_question_object()
     message = await update.effective_message.reply_poll(
         question=quiz_question.question
@@ -65,6 +62,11 @@ async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         , correct_option_id=quiz_question.correct_answer_position
         , is_anonymous=False
         , allows_multiple_answers=True
+        , reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard
+            , one_time_keyboard=False
+            , resize_keyboard=True
+        )
     )
     # Save some info about the poll the bot_data for later use in receive_quiz_answer
     payload = {
@@ -114,16 +116,18 @@ class DefaultConfig:
 
 
 def main():
+    # Create the Application and pass it your bot's token.
     application = Application.builder().token(DefaultConfig.TELEGRAM_TOKEN).build()
-    start_handler = CommandHandler('s', quiz)
+    quiz_handler = CommandHandler('start_quiz', quiz)
     receive_q_answer = PollHandler(DefaultConfig.TOTAL_VOTER_COUNT, receive_quiz_answer)
 
-    application.add_handler(start_handler)
+    application.add_handler(quiz_handler)
     application.add_handler(receive_q_answer)
 
     # log all errors
     application.add_error_handler(error)
 
+    # Run the bot until user press Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
