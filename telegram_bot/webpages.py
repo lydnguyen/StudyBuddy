a29485c7f2fd2bdafb754f43929f527cc2bd970a
lydnguyen <lanyendaonguyen@gmail.com>
2024-09-29 16:59:27 +0200

    add chatid with the all reminder objects

--- telegram_bot/webpages.py
+++ telegram_bot/webpages.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request, render_template_string
 import os
 
@@ -16,41 +18,9 @@ def form():
 @app.route('/submit', methods=['POST'])
 def submit():
     # Get the user input from text fields and message
-    user_data = {
-        "reminder-date": request.form.get('reminder-date'),
-        "day_week": request.form.get('day_week'),
-        "reminder-time": request.form.get('reminder-time'),
-        "all_reminders": request.form.get('all_reminders')
-    }
+    all_reminders = request.form.get('all_reminders')
+    print(json.dumps(all_reminders, indent=4))
 
-    # Get the checkbox values; we use getlist() to retrieve multiple checkbox values
-    interests = request.form.getlist('products')
-    response_1 = f'''
-                <h1>Form Submitted Successfully</h1>
-                <p><strong>reminder date:</strong> {user_data['reminder-date']}</p>
-                <p><strong>week days:</strong> {user_data['day_week']}</p>
-                <p><strong>Interests:</strong> {', '.join(interests) if interests else 'None'}</p>
-                <p><strong>reminder time:</strong> {user_data['reminder-time']}</p>
-                <p><strong>all_reminders:</strong> {user_data['all_reminders']}</p>
-                
-                <button id="close-btn">Close</button>
-                <br><br><a href="/">Go back to form</a>'''
-    response_2 = '''
-                <script>
-                        const CloseBtn = document.getElementById('close-btn');
-                        // Close functionality
-                        CloseBtn.addEventListener('click', function() {
-                            const confirmed = confirm("Are you sure you want to confirm and close the window?");
-                            if (confirmed) {
-                                window.close();
-                                if (!window.closed) {
-                                    alert("Window cannot be closed automatically. Please close the window manually.");
-                                }
-                            }
-                        });
-                </script>
-            '''
-    return response_1+response_2
 
 
 def main():
