674bd29877d3b080272c4f1ccd72bac47b31f7e0
lydnguyen <lanyendaonguyen@gmail.com>
2024-09-30 16:26:45 +0200

    rewrite submits

--- telegram_bot/webpages.py
+++ telegram_bot/webpages.py
@@ -1,7 +1,6 @@
-import json
-
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, jsonify
 import os
+import json
 
 app = Flask(__name__)
 
@@ -18,10 +17,27 @@ def form():
 @app.route('/submit', methods=['POST'])
 def submit():
     # Get the user input from text fields and message
-    all_reminders = request.form.get('all_reminders')
-    print(json.dumps(all_reminders, indent=4))
+    user_data = {
+        "chat_id": request.form.get('chat-info'),
+        "all_reminders": json.loads(request.form.get('all_reminders')),
+    }
+    reminders = []
+    for reminder in user_data['all_reminders']:
+        days = reminder['days'].split(',')
+        topics = reminder['products'].split(',')
+        rem = {
+            'id': reminder['id']
+            , 'week_days': days
+            , 'topics': topics
+            , 'reminder_time': reminder['time']
+            , 'reminder_end_dt': reminder['date']
+        }
+        reminders.append(rem)
 
+    user_data['reminders'] = reminders
+    del user_data['all_reminders']
 
+    return jsonify({'success': True, 'data':user_data}), 200
 
 def main():
     app.run(debug=True)
